346b85a8a943f07916cb9d6369ad5ea2132f5c96
Ashwin Venkatesan <ashwinjv@gmail.com>
2016-11-26 11:47:55 -0800

    chore: change default style and folder structure
    Change default styles from css to scss. Change folder structure to have indidual folders for components. Renamed AppComponent to RootComponent.

--- src/app/components/root/root.component.spec.ts
+++ src/app/components/root/root.component.spec.ts
@@ -1,31 +1,31 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { RootComponent } from './root.component';
 
-describe('AppComponent', () => {
+describe('RootComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        RootComponent
       ],
     });
   });
 
   it('should create the app', async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
+    let fixture = TestBed.createComponent(RootComponent);
     let app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
 
   it(`should have as title 'Passman!'`, async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
+    let fixture = TestBed.createComponent(RootComponent);
     let app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('Passman!');
   }));
 
   it('should render title in a h1 tag', async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
+    let fixture = TestBed.createComponent(RootComponent);
     fixture.detectChanges();
     let compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Passman!');
